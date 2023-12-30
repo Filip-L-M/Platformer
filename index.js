036349225d6ec88d97dc96c07dfd2e17116af045
Filip-L-M <mankowski.fl.work@gmail.com>
2023-12-30 11:53:04 +0100

    Background Sprite

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
+const backgroundLevel1 = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imageSrc: "./img/backgroundLevel1.png",
+});
+
 const player = new Player();
 
 const keys = {
@@ -20,8 +28,8 @@ const keys = {
 
 function animate() {
   window.requestAnimationFrame(animate);
-  c.fillStyle = "green";
-  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  backgroundLevel1.draw();
 
   player.velocity.x = 0;
   if (keys.d.pressed) player.velocity.x = 5;
