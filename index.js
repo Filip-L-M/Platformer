6fa44ca16abd57751fd2989bca0886c233f877eb
Filip-L-M <mankowski.fl.work@gmail.com>
2023-12-30 12:34:27 +0100

    Collision blocks

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
+const parsedCollisions = collisionsLevel1.parse2D();
+const collisionBlocks = parsedCollisions.createObjectsFrom2D();
+
 const backgroundLevel1 = new Sprite({
   position: {
     x: 0,
@@ -30,6 +33,9 @@ function animate() {
   window.requestAnimationFrame(animate);
 
   backgroundLevel1.draw();
+  collisionBlocks.forEach((collisionBlock) => {
+    collisionBlock.draw();
+  });
 
   player.velocity.x = 0;
   if (keys.d.pressed) player.velocity.x = 5;
